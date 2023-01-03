2b0a4bd5951461c658d598cb98314ac52c3d8676
Devsinglad <109593493+Devsinglad@users.noreply.github.com>
2023-01-03 19:58:31 +0100

    init

---
+++ lib/utils/imports/imports.dart
@@ -0,0 +1,8 @@
+export 'package:firebase_core/firebase_core.dart';
+export 'package:flutter/material.dart';
+export 'package:flutter/services.dart';
+export 'package:note/views/screens/loginPage.dart';
+export 'package:cloud_firestore/cloud_firestore.dart';
+export 'package:firebase_auth/firebase_auth.dart';
+export 'package:google_sign_in/google_sign_in.dart';
+export 'package:note/views/screens/HomePage.dart';
